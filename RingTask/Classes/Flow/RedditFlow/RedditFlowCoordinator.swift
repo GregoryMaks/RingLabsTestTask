3305ac6c466ee8fe30c2a6bfe7ecb81d61ef0bc3
Hryhorii Maksiuk <gmaksiuk@swivl.com>
2017-08-07 14:43:24 +0300

    Add UI skeleton

---
+++ RingTask/Classes/Flow/RedditFlow/RedditFlowCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  RedditFlowCoordinator.swift
+//  RingTask
+//
+//  Created by Hryhorii Maksiuk on 8/7/17.
+//  Copyright © 2017 Gregory M. All rights reserved.
+//
+
+import UIKit
+
+
+class RedditFlowCoordinator {
+    
+    let window: UIWindow
+    
+    private var listViewController: RedditPostListViewController?
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let navController = storyboard.instantiateInitialViewController() as? UINavigationController else {
+            print("Error, root navigation not found")
+            return
+        }
+        
+        guard let viewController = navController.viewControllers.first as? RedditPostListViewController else {
+            print("Root view controller not found")
+            return
+        }
+        
+        listViewController = viewController
+        window.rootViewController = listViewController
+    }
+    
+}
